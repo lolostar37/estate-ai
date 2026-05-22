e099579522201b54a65d0db2d21abbb95cb26da8
Ian <fly3737@googlemail.com>
2026-05-22 19:13:54 +0900

    connect molit api

--- app/api/molit/route.ts
+++ app/api/molit/route.ts
@@ -1,10 +1,30 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  return NextResponse.json({
-    success: true,
-    message: 'MOLIT API 연결 성공',
-    test: 'v2',
-    timestamp: Date.now()
-  })
+  try {
+    const key = process.env.MOLIT_API_KEY
+
+    const lawdCode = '11710' // 송파구
+    const dealYmd = '202604'
+
+    const url =
+      `https://apis.data.go.kr/1613000/RTMSDataSvcAptTrade/getRTMSDataSvcAptTrade` +
+      `?serviceKey=${key}` +
+      `&LAWD_CD=${lawdCode}` +
+      `&DEAL_YMD=${dealYmd}`
+
+    const response = await fetch(url)
+
+    const xml = await response.text()
+
+    return NextResponse.json({
+      success: true,
+      data: xml.substring(0,3000)
+    })
+
+  } catch (error) {
+    return NextResponse.json({
+      success: false
+    })
+  }
 }
