052933094fc8ce4529f490f808f7a132e70b79ca
Ian <fly3737@googlemail.com>
2026-05-22 19:17:41 +0900

    add apartment filter

--- app/api/molit/route.ts
+++ app/api/molit/route.ts
@@ -1,10 +1,13 @@
 import { NextResponse } from 'next/server'
+import { XMLParser } from 'fast-xml-parser'
 
 export async function GET() {
+
   try {
+
     const key = process.env.MOLIT_API_KEY
 
-    const lawdCode = '11710' // 송파구
+    const lawdCode = '11710'
     const dealYmd = '202604'
 
     const url =
@@ -17,14 +20,31 @@ export async function GET() {
 
     const xml = await response.text()
 
+    const parser = new XMLParser()
+
+    const json = parser.parse(xml)
+
+    const items =
+      json.response.body.items.item
+
+    const filtered =
+      items.filter(
+        (item:any)=>
+        item.aptNm==="잠실엘스"
+      )
+
     return NextResponse.json({
-      success: true,
-      data: xml.substring(0,3000)
+      success:true,
+      count:filtered.length,
+      data:filtered
     })
 
-  } catch (error) {
+  } catch(error){
+
     return NextResponse.json({
-      success: false
+      success:false
     })
+
   }
+
 }
